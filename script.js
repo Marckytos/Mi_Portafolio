2432ff98baa09a2943e5597a90fc43c67a4a6152
Marckytos <marquillos.bros3451@gmail.com>
2025-03-15 20:58:54 -0600

    Portafolio para mostrar mis proyectos

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('nav a').forEach(enlace => {  //querySelector busca los enlaces <a> dentro de nav, forEach para cada enlace del menú ejecutara una funcion
+    enlace.addEventListener('click', function(e){  //Agrega un evento de clic a cada enlace del menu
+        e.preventDefault();  //evita salto brusco al hacer clic en un enlace del menu
+        const seccion =
+document.querySelector(this.getAttribute('href'));  //get attribute obtiene el valor del href del enlace (ej: #sobre-mi), querySelector busca la seccion correspondiente
+                seccion.scrollIntoView({behavior: 
+'smooth'});  //Hace que la seccion se desplace suavemente en la pantalla
+    });
+});
+
+const btnSubir =
+document.getElementById('btnSubir');  //Busca el boton con id = 'btnSubir' y lo guarda en la variable btnSubir
+
+window.addEventListener('scroll', function () {  //Detecta cuando el usuario hace scroll en la pagina
+    if (this.window.scrollY > 200){ 
+        btnSubir.style.display = 'block';  //Si el usuario ha bajado más de 200 pixeles, muestra el boton
+    } else {
+        btnSubir.style.display = 'none';  //Si el usuario esta en la parte de arriba, oculta el boton
+    }
+});
+btnSubir.addEventListener('click', function(){  //Cuando se haga clic, el boton ejecutara el codigo dentro
+    window.scrollTo({top: 0, behavior: 'smooth'});
+});  //Hace que la pagina se desplace suavemente hacia arriba
